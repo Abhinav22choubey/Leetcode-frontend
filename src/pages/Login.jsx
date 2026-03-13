67bb4a0d7cca259650bc5ca999d7526756e0c876
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2026-03-13 05:58:38 +0530

    added validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 
-const Login = () => {
+const schema = z.object({
+    emailId: z.string().email(),
+    password: z
+        .string()
+        .min(8, "Minimum 8 characters")
+        .regex(/[A-Z]/, "Must contain one uppercase letter")
+        .regex(/[a-z]/, "Must contain one lowercase letter")
+        .regex(/[0-9]/, "Must contain one number")
+        .regex(/[^A-Za-z0-9]/, "Must contain one special character"),
+})
+
+const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(schema),
+    })
+    const submittedForm = (data) => {
+        console.log(data);
+    }
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <form onSubmit={handleSubmit(submittedForm)}>
+                <input type='email' {...register("emailId")} />
+                {errors.emailId && (<span>{errors.emailId.message}</span>)}
+                <input type='password' {...register("password")} />
+                {errors.password && (<span>{errors.password.message}</span>)}
+                <button type='submit'>Submit</button>
+            </form>
+        </>
     );
 }
 
-export default Login;
+export default SignUp;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+    firstName: z.string().min(3, "Name is too short"),
+    emailId: z.string().email(),
+    password: z
+        .string()
+        .min(8, "Minimum 8 characters")
+        .regex(/[A-Z]/, "Must contain one uppercase letter")
+        .regex(/[a-z]/, "Must contain one lowercase letter")
+        .regex(/[0-9]/, "Must contain one number")
+        .regex(/[^A-Za-z0-9]/, "Must contain one special character"),
+})
 
 const SignUp = () => {
     const {
@@ -7,16 +21,21 @@ const SignUp = () => {
         handleSubmit,
         watch,
         formState: { errors },
-    } = useForm()
-    const submittedForm=(data)=>{
+    } = useForm({
+        resolver: zodResolver(schema),
+    })
+    const submittedForm = (data) => {
         console.log(data);
     }
     return (
         <>
             <form onSubmit={handleSubmit(submittedForm)}>
                 <input {...register("firstName")} />
-                <input {...register("emailId")} />
-                <input {...register("password")} />
+                {errors.firstName && (<span>{errors.firstame.message}</span>)}
+                <input type='email' {...register("emailId")} />
+                {errors.emailId && (<span>{errors.emailId.message}</span>)}
+                <input type='password' {...register("password")} />
+                {errors.password && (<span>{errors.password.message}</span>)}
                 <button type='submit'>Submit</button>
             </form>
         </>
