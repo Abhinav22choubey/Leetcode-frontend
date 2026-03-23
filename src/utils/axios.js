8a6a37c0a58e4a5a22c0136108a37c4f4d64315b
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2026-03-23 23:03:37 +0530

    added api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosMain = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://leetcode-backend-smd2.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
